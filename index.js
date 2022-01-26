cc3d935b6fa82240f25c046d4d8434bb138bd92a
applebee <applebee@applebee1558.com>
2022-01-25 22:24:20 -0800

    handle multiple activities

--- index.js
+++ index.js
@@ -11,36 +11,47 @@ client.on('ready', () => {
 
 client.on('presenceUpdate', async (oldMember, newMember) => {
     const role = newMember.guild.roles.cache.get("932784788115427348");
-    const activities = newMember.activities[1];
+
+
+    let using_cider = false
+    for (const activity of newMember.activities){
+        // 911790844204437504 - Cider
+        // 886578863147192350 - Apple Music
+
+        if (activity && (activity.applicationId === ( "911790844204437504" ) || (activity.applicationId === ( "886578863147192350" )))) {
+            try {
+                let listenerinfo = {
+                    userid: newMember.userId,
+                    userName: newMember.member.user.username,
+                    songName: activity.details
+                }
+
+                if (newMember.member._roles.includes("932784788115427348")) { // user already has listening role, no need to change roles
+                    console.log("\x1b[2m", "Listener updated -", listenerinfo)
+                    return // not changing any roles, just a log
+                } else {
+                    console.log('\x1b[35m%s\x1b[0m', "Listener added -", listenerinfo)
+                    using_cider = true // code below will handle it
+                    break
+                }
+         
+            } catch(e) {
+                console.log("An error occurred. ",e)
+            }
+        }
+    }
     
-    // 911790844204437504 - Cider
-    // 886578863147192350 - Apple Music
+    if(using_cider){
+        newMember.member.roles.add(role) // add listening on cider role
 
-    if (activities && (activities.applicationId === ( "911790844204437504" ) || (activities.applicationId === ( "886578863147192350" )))) {
-        try {
-            let listenerinfo = {
-                userid: newMember.userId,
-                userName: newMember.member.user.username,
-                songName: activities.details
-            }
 
-            if (newMember.member._roles.includes("932784788115427348")) {
-                console.log("\x1b[2m", "Listener updated -", listenerinfo)
-            } else {
-                console.log('\x1b[35m%s\x1b[0m', "Listener added -", listenerinfo)
-                return newMember.member.roles.add(role)
-            }
-
-            if (!newMember.member._roles.includes("932816700305469510")) {
-                newMember.member.roles.add("932816700305469510") // Add Cider User role.
-            }
-        } catch(e) {
-            console.log("An error occurred. ",e)
+        if (!newMember.member._roles.includes("932816700305469510")) {
+            newMember.member.roles.add("932816700305469510") // Add Cider User role.
         }
     } else { // Remove role if exists or ignore.
         try {
             if (newMember.member._roles.includes("932784788115427348")) {
-                newMember.member.roles.remove("932784788115427348");
+                newMember.member.roles.remove("932784788115427348"); // remove listening on cider role
                 let rmlistenerinfo = {
                     userid: newMember.userId,
                     userName: newMember.member.user.username,
