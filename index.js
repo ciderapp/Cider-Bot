aba05d86f77f0b14a78f1881a530eef521760fec
Keefe Eilish <vinamra.jain07@outlook.com>
2022-02-11 20:41:42 +0530

    Cleaning intents
    Making the intent declaration shorter because yes.

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const auth = require('./tokens.json');
 const cheerio = require('cheerio');
 const fetch = require('node-fetch');
 const deploy = require('./deploy-commands.js');
-const client = new Discord.Client({
-    intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES, Discord.Intents.FLAGS.GUILD_PRESENCES, Discord.Intents.FLAGS.GUILD_MEMBERS]
-});
+const client = new Discord.Client({ intents: 641 });
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag} at`);
