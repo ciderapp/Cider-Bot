183328b760aceff1b71ce2ab9352d95abf89f523
cryptofyre <cryptofyre@cryptofyre.org>
2022-03-02 08:47:19 -0600

    Push sauce component changes.

--- index.js
+++ index.js
@@ -148,6 +148,36 @@ client.on('interactionCreate', async interaction => {
            
             if (typeof interaction.options.getBoolean('show') == 'undefined') { show = false } else { show = interaction.options.getBoolean('show') }
             await interaction.reply({ content: `Listing available macOS installation packages.`, ephemeral: !show, components: [buttons]})
+        } else {
+            if (commandName === 'sauceme') {
+                let saucerequest = await fetch('https://api.waifu.im/random/?selected_tags=hentai')
+                let sauce = await saucerequest.json()
+                let buttons = new Discord.MessageActionRow()
+                let saucecontent = await sauce.images[0].url;
+                let sauceart = await sauce.images[0].source;
+                let saucecolor = await sauce.images[0].dominant_color;
+                buttons.addComponents(
+                    new Discord.MessageButton()
+                    .setLabel("Open in Browser")
+                    .setStyle('LINK')
+                    .setURL(saucecontent.toString())
+                )
+                buttons.addComponents(
+                    new Discord.MessageButton()
+                    .setLabel("Open Artist/Source in Browser")
+                    .setStyle('LINK')
+                    .setURL(sauceart.toString())
+                )
+                let embed = new Discord.MessageEmbed()
+                    .setColor(saucecolor.toString())
+                    .setTitle("Sauce Randomizer")
+                    .setURL(saucecontent.toString())
+                    .setFooter({ text: "Requested by " + interaction.member.user.username, iconURL: interaction.member.user.avatarURL() })
+                    .setImage(saucecontent.toString())
+                    .setTimestamp()
+
+                await interaction.reply({ content: `feeling down bad are we?`, embeds: [embed], ephemeral: true, components: [buttons]})
+            }
         }
     }
 })
