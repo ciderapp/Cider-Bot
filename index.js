7fcee90da427b56c702c833ab08d9fa709be279c
child_duckling <19170969+child-duckling@users.noreply.github.com>
2022-03-19 00:54:49 -0700

    presense checking if in different guild

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const client = new Discord.Client({
     intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES, Discord.Intents.FLAGS.GUILD_PRESENCES, Discord.Intents.FLAGS.GUILD_MEMBERS]
 });
 
+let cider_guild = "843954443845238864"
+
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag} at`);
     console.log(Date())
@@ -16,55 +18,63 @@ client.on('ready', () => {
 
 
 client.on('presenceUpdate', async(oldMember, newMember) => {
-    const role = newMember.guild.roles.cache.get("932784788115427348");
-    let using_cider = false
-    for (const activity of newMember.activities) {
-        // 911790844204437504 - Cider
-        // 886578863147192350 - Apple Music
-        if (activity && (activity.applicationId === ("911790844204437504") || (activity.applicationId === ("886578863147192350")))) {
-            let listenerinfo = {
-                userid: newMember.userId,
-                userName: newMember.member.user.username,
-                songName: activity.details
-            }
-
-            if (newMember.member._roles.includes("932784788115427348")) { // user already has listening role, no need to change roles
-                console.log("\x1b[2m", "Listener updated -", listenerinfo)
-                return // not changing any roles, just a log
-            } else {
-                console.log('\x1b[35m%s\x1b[0m', "Listener added -", listenerinfo)
-                using_cider = true // code below will handle it
-                break
-            }
-        }
-    }
-    if (using_cider) {
-        try {
-            newMember.member.roles.add(role) // add listening on cider role
-            if (!newMember.member._roles.includes("932816700305469510")) {
-                try { newMember.member.roles.add("932816700305469510") } catch (e) { console.log("An error occurred while adding role. ", e) } // Add Cider User role.
-            }
-        } catch (e) {
-            console.log("An error occurred. ", e)
-        }
-
-    } else { // Remove role if exists or ignore.
-        try {
-            if (newMember.member._roles.includes("932784788115427348")) {
-                try {
-                    newMember.member.roles.remove("932784788115427348"); // remove listening on cider role
-                } catch (e) { console.log("An error occurred on role removal. ", e) }
-                let rmlistenerinfo = {
+    try { //If role not found in guild, do nothing.
+        const role = newMember.guild.roles.cache.get("932784788115427348");
+        let using_cider = false
+        for (const activity of newMember.activities) {
+            // 911790844204437504 - Cider
+            // 886578863147192350 - Apple Music
+            if (activity && (activity.applicationId === ("911790844204437504") || (activity.applicationId === ("886578863147192350")))) {
+                let listenerinfo = {
                     userid: newMember.userId,
                     userName: newMember.member.user.username,
-                    dateRemoved: Date()
+                    songName: activity.details
                 }
-                console.log("\x1b[33m%s\x1b[0m", "Listener removed -", rmlistenerinfo)
+
+                if (newMember.member._roles.includes("932784788115427348")) { // user already has listening role, no need to change roles
+                    console.log("\x1b[2m", "Listener updated -", listenerinfo)
+                    return // not changing any roles, just a log
+                } else {
+                    console.log('\x1b[35m%s\x1b[0m', "Listener added -", listenerinfo)
+                    using_cider = true // code below will handle it
+                    break
+                }
             }
-        } catch (e) {
-            console.log(e)
         }
-    }
+        if (using_cider) {
+            try {
+                newMember.member.roles.add(role) // add listening on cider role
+                if (!newMember.member._roles.includes("932816700305469510")) {
+                    try {
+                        newMember.member.roles.add("932816700305469510")
+                    } catch (e) {
+                        console.log("An error occurred while adding role. ", e)
+                    } // Add Cider User role.
+                }
+            } catch (e) {
+                console.log("An error occurred. ", e)
+            }
+
+        } else { // Remove role if exists or ignore.
+            try {
+                if (newMember.member._roles.includes("932784788115427348")) {
+                    try {
+                        newMember.member.roles.remove("932784788115427348"); // remove listening on cider role
+                    } catch (e) {
+                        console.log("An error occurred on role removal. ", e)
+                    }
+                    let rmlistenerinfo = {
+                        userid: newMember.userId,
+                        userName: newMember.member.user.username,
+                        dateRemoved: Date()
+                    }
+                    console.log("\x1b[33m%s\x1b[0m", "Listener removed -", rmlistenerinfo)
+                }
+            } catch (e) {
+                console.log(e)
+            }
+        }
+    } catch (e) {}
 })
 
 client.on('messageCreate', async message => {
