3d9252ea165ea4a087ec89e0babd90a89073eea9
child_duckling <19170969+quacksire@users.noreply.github.com>
2022-03-31 21:25:15 -0700

    env fix

--- local.js
+++ local.js
@@ -1,33 +1,33 @@
 const isRailway = require('is-railway');
-const config = require('./tokens.json');
+//require('./tokens.json')
 
 module.exports = {
     token: function () {
         if (isRailway()) {
             return process.env.TOKEN;
         } else {
-            return config.token;
+            return require('./tokens.json').token;
         }
     },
     mongo: function () {
         if (isRailway()) {
             return `mongodb://${process.env.MONGOUSER}:${ process.env.MONGOPASSWORD }@${ process.env.MONGOHOST }:${ process.env.MONGOPORT }`;
         } else {
-            return config.mongo;
+            return require('./tokens.json').mongo;
         }
     },
     port: function () {
         if (isRailway()) {
             return process.env.PORT;
         } else {
-            return config.port;
+            return require('./tokens.json').port;
         }
     },
     expressurl: function () {
         if (isRailway()) {
             return process.env.URL;
         } else {
-            return `${config.url}:${config.port}`;
+            return `${require('./tokens.json').url}:${require('./tokens.json').port}`;
         }
     },
     auth: {
@@ -35,14 +35,14 @@ module.exports = {
             if (isRailway()) {
                 return process.env.AUTH_CLIENTID;
             } else {
-                return config.auth.clientId;
+                return require('./tokens.json').auth.clientId;
             }
         },
         clientSecret: function () {
             if (isRailway()) {
                 return process.env.AUTH_CLIENTSECRET;
             } else {
-                return config.auth.clientSecret;
+                return require('./tokens.json').auth.clientSecret;
             }
         },
     }
