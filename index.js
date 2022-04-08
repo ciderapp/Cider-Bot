2fe8202381694d8cbce9cb0242fe1e334ae3bdf8
yazninja <yazlesean@gmail.com>
2022-04-08 17:22:29 +0800

    text v2

--- index.js
+++ index.js
@@ -119,7 +119,7 @@ client.on('messageCreate', async message => {
         const embed = new Discord.MessageEmbed()
         .setColor('#fb003f')
         .setTitle("Why is Cider Slow?")
-        .setDescription("Cider is slow because its not taking full advantage of your hardware. To turn on, do :KeyCtrl: (or ⌘) :KeyComma: <a:righter_arrow:509735362994896924> “Advanced” <a:righter_arrow:509735362994896924> “Enable Hardware Acceleration” <a:righter_arrow:509735362994896924> “WebGPU”")
+        .setDescription("Cider is slow because its not taking full advantage of your hardware. To turn on, do <:KeyCtrl:830276580835721239> (or ⌘) <:KeyComma:830276581036523561> <a:righter_arrow:509735362994896924> “Advanced” <a:righter_arrow:509735362994896924> “Enable Hardware Acceleration” <a:righter_arrow:509735362994896924> “WebGPU”")
         message.reply({ embeds: [embed] }).then(reply => {
             setTimeout(() => reply.delete(), 12000)
         })
