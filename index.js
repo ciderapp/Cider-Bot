2ca28bb435b08a1ae9d1ab952060c1e1c8f25ac3
yazninja <yazlesean@gmail.com>
2022-04-08 12:58:50 +0800

    Delete reply and message after n seconds

--- index.js
+++ index.js
@@ -118,7 +118,10 @@ client.on('messageCreate', async message => {
         .setColor('#fb003f')
         .setTitle("Lossless Audio in Cider")
         .setDescription("Lossless playback is not currently supported in Cider. Apple's MusicKit Framework does have lossless support, however, decryption of this audio is not supported.")
-        message.reply({ embeds: [embed], ephemeral: true })
+        message.reply({ embeds: [embed] }).then(reply => {
+            setTimeout(() => reply.delete(), 12000)
+        })
+        setTimeout(() => message.delete(), 13000)
     } else if (message.content.match(/^(?!cider:\/\/).+(music\.apple\.com)([^\s]+)/gi)) {
         const link = message.content.match(/^(?!cider:\/\/).+(music\.apple\.com)([^\s]+)/gi)
         console.log("[Link] Creating redirect embed.")
