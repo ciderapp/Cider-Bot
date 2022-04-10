27ce2b356447d354bb5b944feca4f670d25306b9
yazninja <yazlesean@gmail.com>
2022-04-11 00:56:31 +0800

    use nightly instead

--- interactions/branch.js
+++ interactions/branch.js
@@ -31,19 +31,20 @@ module.exports = {
             }
         }
         console.log(interaction.member);
-        if (interaction.member._roles.includes('848363050205446165') || interaction.member._roles.includes('875082121427955802')) {
-            if (buttons.components.length == 0) {
-                await interaction.reply({ content: `I have failed to retrieve any installers from the **${branch}** branch.`, ephemeral: !show })
-            } else {
-                await interaction.reply({ content: `${user}, What installer do you want from the **${branch}** branch?`, components: [buttons] })
-            }
-        }
-        else {
-            if (buttons.components.length == 0) {
-                await interaction.reply({ content: `I have failed to retrieve any installers from the **${branch}** branch.`, ephemeral: !show })
-            } else {
-                await interaction.reply({ content: `What installer do you want from the **${branch}** branch?`, ephemeral: !show, components: [buttons] })
-            }
-        }
+        // if (interaction.member._roles.includes('848363050205446165') || interaction.member._roles.includes('875082121427955802')) {
+        //     if (buttons.components.length == 0) {
+        //         await interaction.reply({ content: `I have failed to retrieve any installers from the **${branch}** branch.`, ephemeral: !show })
+        //     } else {
+        //         await interaction.reply({ content: `${user}, What installer do you want from the **${branch}** branch?`, components: [buttons] })
+        //     }
+        // }
+        // else {
+        //     if (buttons.components.length == 0) {
+        //         await interaction.reply({ content: `I have failed to retrieve any installers from the **${branch}** branch.`, ephemeral: !show })
+        //     } else {
+        //         await interaction.reply({ content: `What installer do you want from the **${branch}** branch?`, ephemeral: !show, components: [buttons] })
+        //     }
+        // }
+        await interaction.reply({ content: `Command has been disabled, please use \`/nightly\` instead.` })
     }
 }
