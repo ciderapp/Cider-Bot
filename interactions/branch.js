f44aa1aed7b78a940bed80c8e1c427c83365f308
yazninja <yazlesean@gmail.com>
2022-04-10 22:57:25 +0800

    mthrfckin railway always sets to 0.0.0.0:80

--- interactions/branch.js
+++ interactions/branch.js
@@ -32,6 +32,7 @@ module.exports = {
                 break;
             }
         }
+        
         if (show && (interaction.member._roles.includes('848363050205446165') || interaction.member._roles.includes('875082121427955802'))) {
             if (buttons.components.length == 0) {
                 await interaction.reply({ content: `I have failed to retrieve any installers from the **${branch}** branch.`, ephemeral: !show })
