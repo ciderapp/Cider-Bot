0d3e1214952ecd1c5c66e587c4d8bd1412525ba2
yazninja <yazlesean@gmail.com>
2022-04-12 21:20:59 +0800

    add restrictions command

---
+++ commands/restrictions.js
@@ -0,0 +1,22 @@
+const { SlashCommandBuilder} = require('@discordjs/builders');
+const Discord = require("discord.js");
+module.exports = {
+    data: new SlashCommandBuilder().setName('restrictions').setDescription('Responds to \"Why is my Cider skipping songs\"').addUserOption(option => option.setName('user').setDescription('User to repond to')),
+    async execute(interaction) {
+        let embed = new Discord.MessageEmbed()
+            .setTitle("Why is Cider skipping some songs?")
+            .setDescription("Your account might have content restartictions set to \"Clean\"\n\n In order to check, go to https://tv.apple.com/settings and check the \"Content Restrictions\" section.\n\n Make sure that Music is set to \`Explicit\`")
+            .setTimestamp()
+        let user = interaction.options.getUser('user') || null
+        let settings = new Discord.MessageButton()
+            .setLabel(`Apple TV Settings (yes, its tv)`)
+            .setStyle('LINK')
+            .setURL(`https://tv.apple.com/settings`)
+        if (user) {
+            await interaction.reply({ content: `${user}`, embeds: [embed], components: [new Discord.MessageActionRow().addComponents([settings])] })
+        } else {
+            await interaction.reply({ embeds: [embed], components: [new Discord.MessageActionRow().addComponents([settings])] })
+        }
+        
+    },
+};
