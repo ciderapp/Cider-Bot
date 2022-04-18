047edfb3828104eefc50067c41e30e8f894e74bf
Amaru8 <52407090+Amaru8@users.noreply.github.com>
2022-04-18 20:42:36 +0200

    Fixed typo in restrictions

--- commands/restrictions.js
+++ commands/restrictions.js
@@ -5,7 +5,7 @@ module.exports = {
     async execute(interaction) {
         let embed = new Discord.MessageEmbed()
             .setTitle("Why is Cider skipping some songs?")
-            .setDescription("Your account might have content restartictions set to \"Clean\"\n\n In order to check, go to [Apple Media Account settings](https://tv.apple.com/settings) and check the \"Content Restrictions\" section.\n\n Make sure that Music is set to \`Explicit\`")
+            .setDescription("Your account might have content restrictions set to \"Clean\"\n\n In order to check, go to [Apple Media Account settings](https://tv.apple.com/settings) and check the \"Content Restrictions\" section.\n\n Make sure that Music is set to \`Explicit\`")
             .setFooter({ text: "Requested by " + interaction.member.user.username, iconURL: interaction.member.user.avatarURL() })
             .setTimestamp()
         let user = interaction.options.getUser('user') || null
@@ -18,6 +18,5 @@ module.exports = {
         } else {
             await interaction.reply({ embeds: [embed] })
         }
-        
     },
 };
