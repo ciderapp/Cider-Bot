422aa928c42d7389168c108fadde754bdcc19c93
yazninja <yazlesean@gmail.com>
2022-04-25 14:34:12 +0800

    put back roles, testing is done, final

--- commands/faqupdate.js
+++ commands/faqupdate.js
@@ -4,8 +4,7 @@ module.exports = {
     data: new SlashCommandBuilder().setName('faqupdate').setDescription('Updates the FAQ page"')
         .addBooleanOption(option => option.setName('publish').setDescription('set to true to publish in FAQ channel').setRequired(false)),
     async execute(interaction) {
-        // if (interaction.member._roles.includes('848363050205446165')) {
-            if (true) {
+        if (interaction.member._roles.includes('848363050205446165')) {
             let publish = interaction.options.getBoolean('publish') || false
             if (publish) {
                 interaction.member.guild.channels.cache.get("911395772803735612").send({ embeds: faqEmbed.embeds })
