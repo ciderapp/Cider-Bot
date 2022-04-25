7d99eb088b00050c5af1db886a49679d47a17461
yazninja <yazlesean@gmail.com>
2022-04-25 14:18:37 +0800

    fix faqupdate

--- commands/faqupdate.js
+++ commands/faqupdate.js
@@ -4,10 +4,11 @@ module.exports = {
     data: new SlashCommandBuilder().setName('faqupdate').setDescription('Updates the FAQ page"')
         .addBooleanOption(option => option.setName('publish').setDescription('set to true to publish in FAQ channel').setRequired(false)),
     async execute(interaction) {
+        console.log(interaction)
         if (interaction.member._roles.includes('848363050205446165')) {
             let publish = interaction.options.getBoolean('publish') || false
             if (publish) {
-                client.channels.cache.get("911395772803735612").send({ embeds: faqEmbed.embeds })
+                interaction.member.guild.channels.cache.get("911395772803735612").send({ embeds: faqEmbed.embeds })
                 interaction.reply({ content: '<#911395772803735612> page updated!' })
             }
             else { interaction.reply({ content: `To publish, set publish to \`true\``, embeds: faqEmbed.embeds }) }
