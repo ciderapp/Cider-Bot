7534e71fb1dccda18fcfa3f39b410b6db38dbc65
Maikiwi <stella@mai.kiwi>
2022-05-06 10:18:46 -0700

    Fix apple's broken year display

--- index.js
+++ index.js
@@ -204,7 +204,7 @@ client.on('messageCreate', async message => {
                     const $ = cheerio.load(html)
                     const title = $('meta[property="og:title"]').attr('content') || $('title').text() || $('meta[name="title"]').attr('content')
                     const description = $('meta[property="twitter:description"]').attr('content') || $('meta[name="twitter:description"]').attr('content')
-                    const image = $('meta[property="og:image"]').attr('content') || $('meta[property="og:image:url"]').attr('content')
+                    const image = $('meta[property="og:image"]').attr('content').replace("年年", "年") || $('meta[property="og:image:url"]').attr('content').replace("年年", "年")
                     const modlink = link[0].replace('https://', '')
                     const play_link = "https://cider.sh/p?" + modlink
                     const view_link = "https://cider.sh/o?" + modlink
