c437ded29e0fa98dcd5541a9c1df0d0ad2b0ee2b
yazninja <yazlesean@gmail.com>
2022-05-17 21:10:05 +0800

    check for moderatable

--- commands/modcmds.js
+++ commands/modcmds.js
@@ -45,31 +45,34 @@ module.exports = {
     async execute(interaction) {
         if (interaction.member._roles.includes('848363050205446165') || interaction.member._roles.includes('875082121427955802')) {
             let user = interaction.options.getUser('user');
-            
-            if (interaction.options.getSubcommand() === 'kick') {
-                let reason = interaction.options.getString('reason');
-                interaction.guild.members.cache.get(user.id).send(`You have been kicked from **${interaction.guild.name}** for: *${reason}*`);
-                interaction.guild.members.cache.get(user.id).kick(reason);
-                interaction.reply({ content: `${user} has been kicked.` });
-            }
-            else if (interaction.options.getSubcommand() === 'ban') {
-                let reason = interaction.options.getString('reason');
-                await interaction.guild.members.cache.get(user.id).timeout(86400000 * 28, reason);
-                interaction.member.guild.channels.cache.get("972138658230579210").send({ content: `<@191621342473224192>, A Mod has requested a ban for ${user}\nReason: ${reason}\nRequested by: ${interaction.user}` })
-                interaction.guild.members.cache.get(user.id).send(`You have are muted and have been screened to a ban from **${interaction.guild.name}** with the reason of: *${reason}*`);
-                interaction.reply({ content: `${user} has been screened for a ban.` });
-            }
-            else if (interaction.options.getSubcommand() === 'mute') {
-                let reason = interaction.options.getString('reason');
-                let time = interaction.options.getNumber('time');
-                await interaction.guild.members.cache.get(user.id).timeout(time * 60 * 1000, reason);
-                interaction.guild.members.cache.get(user.id).send(`You have been muted for **${time}** minutes for the following reason: *${reason}*`);
-                interaction.reply({ content: `${user} has been muted for ${time} minutes.` });
-            }
-            else if (interaction.options.getSubcommand() === 'unmute') {
-                await interaction.guild.members.cache.get(user.id).timeout(0);
-                interaction.guild.members.cache.get(user.id).send(`You have been unmuted`);
-                interaction.reply({ content: `${user} has been unmuted.` });
+            if(user.moderatable) {
+                if (interaction.options.getSubcommand() === 'kick') {
+                    let reason = interaction.options.getString('reason');
+                    interaction.guild.members.cache.get(user.id).send(`You have been kicked from **${interaction.guild.name}** for: *${reason}*`);
+                    interaction.guild.members.cache.get(user.id).kick(reason);
+                    interaction.reply({ content: `${user} has been kicked.` });
+                }
+                else if (interaction.options.getSubcommand() === 'ban') {
+                    let reason = interaction.options.getString('reason');
+                    await interaction.guild.members.cache.get(user.id).timeout(86400000 * 28, reason);
+                    interaction.member.guild.channels.cache.get("972138658230579210").send({ content: `<@191621342473224192>, A Mod has requested a ban for ${user}\nReason: ${reason}\nRequested by: ${interaction.user}` })
+                    interaction.guild.members.cache.get(user.id).send(`You have are muted and have been screened to a ban from **${interaction.guild.name}** with the reason of: *${reason}*`);
+                    interaction.reply({ content: `${user} has been screened for a ban.` });
+                }
+                else if (interaction.options.getSubcommand() === 'mute') {
+                    let reason = interaction.options.getString('reason');
+                    let time = interaction.options.getNumber('time');
+                    await interaction.guild.members.cache.get(user.id).timeout(time * 60 * 1000, reason);
+                    interaction.guild.members.cache.get(user.id).send(`You have been muted for **${time}** minutes for the following reason: *${reason}*`);
+                    interaction.reply({ content: `${user} has been muted for ${time} minutes.` });
+                }
+                else if (interaction.options.getSubcommand() === 'unmute') {
+                    await interaction.guild.members.cache.get(user.id).timeout(0);
+                    interaction.guild.members.cache.get(user.id).send(`You have been unmuted`);
+                    interaction.reply({ content: `${user} has been unmuted.` });
+                }
+            } else{
+                interaction.reply({ content: `${user.username} is too powerful.` });
             }
         } else {
             interaction.reply({ content: 'You do not have permissions to use this command.', type: 'error', ephemeral: true });
