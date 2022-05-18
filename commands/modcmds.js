abb98b0b57b959d899693d51f2f80421ba05cabc
yazninja <yazlesean@gmail.com>
2022-05-18 11:49:46 +0800

    use admin channel

--- commands/modcmds.js
+++ commands/modcmds.js
@@ -42,6 +42,7 @@ module.exports = {
                 .setRequired(true))),
 
     async execute(interaction) {
+        let adminchannel = "932110086929780777";
         if (interaction.member._roles.includes('848363050205446165') || interaction.member._roles.includes('875082121427955802')) {
             let user = interaction.options.getUser('user');
             if(interaction.guild.members.cache.get(user.id).moderatable) {
@@ -54,7 +55,7 @@ module.exports = {
                 else if (interaction.options.getSubcommand() === 'ban') {
                     let reason = interaction.options.getString('reason');
                     await interaction.guild.members.cache.get(user.id).timeout(86400000 * 28, reason);
-                    interaction.member.guild.channels.cache.get("972138658230579210").send({ content: `<@191621342473224192>, A Mod has requested a ban for ${user}\nReason: ${reason}\nRequested by: ${interaction.user}` })
+                    interaction.member.guild.channels.cache.get(adminchannel).send({ content: `<@191621342473224192>, A Mod has requested a ban for ${user}\nReason: ${reason}\nRequested by: ${interaction.user}` })
                     interaction.guild.members.cache.get(user.id).send(`You have are muted and have been screened to a ban from **${interaction.guild.name}** with the reason of: *${reason}*`);
                     interaction.reply({ content: `${user} has been screened for a ban.` });
                 }
