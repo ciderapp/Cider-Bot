e958476ca3d1079bcd572ed214f66276529246a2
yazninja <yazlesean@gmail.com>
2022-05-24 18:27:11 +0800

    embed is an array :/

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ client.on('presenceUpdate', async (oldMember, newMember) => {
                     })
                     let mentionEmbed = new Discord.MessageEmbed()
                     .setDescription(`Hi <@${user.userid}>, instead of listening to \`${lasttrack.song} by ${lasttrack.artist}\` on Spotify, try playing it on [Cider](${lasttrack.url})`)
-                    guild.channels.cache.get("976834125719818300").send({embeds: mentionEmbed})
+                    guild.channels.cache.get("976834125719818300").send({embeds: [mentionEmbed] })
                 }
             })
         }
