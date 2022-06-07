ab1120a502abd9471ee4c171ad3b9e0222157375
yazninja <yazlesean@gmail.com>
2022-06-07 15:32:32 +0800

    ping v3

--- commands/ping.js
+++ commands/ping.js
@@ -5,13 +5,13 @@ module.exports = {
     .setName("ping")
     .setDescription("Get the bots latency!"),
   execute: async (interaction) => {
-    await interaction.reply({ content: 'Pinging...' })
+    await interaction.reply({ embeds:[{color: "#0099ff", description: "Pinging ..."}] })
     consola.info(interaction)
     let embed = new MessageEmbed()
       .setColor("#0099ff")
       .setTitle("Pong!🏓")
       .setDescription(`API: \`${interaction.client.ws.ping}ms\`\nLatency: \`${Date.now() - interaction.createdTimestamp}ms\``);
 
-    await interaction.editReply({ content: "", embeds:[embed] });
+    await interaction.editReply({ content: " ", embeds:[embed] });
   },
 };
