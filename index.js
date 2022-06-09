1b2c87ef6017efb6ffba6a41d6309b77456169b2
yazninja <yazlesean@gmail.com>
2022-06-09 23:56:08 +0800

    make it only happen in cider server

--- index.js
+++ index.js
@@ -176,7 +176,7 @@ client.on('messageCreate', async message => {
     });
     if (message.author.bot) return
 
-    if(!message.member._roles.includes("848363050205446165")) // if not dev team
+    if(message.member.guild.id == "843954443845238864" && !message.member._roles.includes("848363050205446165")) // if not dev team
     {
         if (message.content.match(profanityFilter)) {
             message.reply({ content: `${message.author} Hey, that's some spicy vocabulary you got there.  It has no place in this Christian family friendly server.  Try here instead: https://discord.gg/fNXzTB9FtW`})
