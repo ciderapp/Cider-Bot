0bfeedbb312f0af1db66bb830396245e0020a138
yazninja <71800112+yazninja@users.noreply.github.com>
2022-06-16 01:22:44 +0800

    lmao

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,15 +1,12 @@
-import 'dotenv/config';
+import { token as auth, clientId } from './local.js';
 import { readdirSync } from 'fs';
 import { REST } from '@discordjs/rest'
 import { Routes } from 'discord-api-types/v9'
 
-let auth = process.env.bot_token
-let clientId = process.env.bot_clientId
-
 const commands = [];
 const commandFiles = readdirSync('./commands').filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
-    const { command } = await import(`./commands/${file}`);
+    const { command }= await import(`./commands/${file}`);
     commands.push(command.data.toJSON());
 }
 
@@ -23,5 +20,7 @@ const rest = new REST({ version: '9' }).setToken(auth);
             { body: commands },
         );
         console.log('Successfully reloaded application (/) commands.\n' + commands.map(c => c.name).join('\n'));
-    } catch (error) { console.error(error); }
+    } catch (error) {
+        console.error(error);
+    }
 })();
