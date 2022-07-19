4ad992ed7e700bb4939f9df69746d404140036d9
yazninja <71800112+yazninja@users.noreply.github.com>
2022-07-19 18:27:21 +0800

    Update deploy-commands to use rest api v10

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,7 +1,7 @@
-import { token as auth, clientId } from './local.js';
+import { token, clientId } from './local.js';
 import { readdirSync } from 'fs';
 import { REST } from '@discordjs/rest'
-import { Routes } from 'discord-api-types/v9'
+import { Routes } from 'discord.js'
 
 const commands = [];
 const commandFiles = readdirSync('./commands').filter(file => file.endsWith('.js'));
@@ -10,15 +10,12 @@ for (const file of commandFiles) {
     commands.push(command.data.toJSON());
 }
 
-const rest = new REST({ version: '9' }).setToken(auth);
+const rest = new REST({ version: '10' }).setToken(token);
 
 (async () => {
     try {
         console.log('Started refreshing application (/) commands.');
-        await rest.put(
-            Routes.applicationCommands(clientId),
-            { body: commands },
-        );
+        await rest.put(Routes.applicationCommands(clientId),{ body: commands });
         console.log('Successfully reloaded application (/) commands.\n' + commands.map(c => c.name).join('\n'));
     } catch (error) {
         console.error(error);
