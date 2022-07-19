61768fa51ae1bf0acf5577305ce562136084f101
yazninja <yazlesean@gmail.com>
2022-07-19 20:39:21 +0800

    disable link embeds

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -19,99 +19,100 @@ export const event = {
         });
         if (message.author.bot) return;
 
-        /* Change Apple Music Link */
-        if (message.content.match(/^(?!cider:\/\/).+(music\.apple\.com)([^\s]+)/gi)) {
-            const link = message.content.match(/^(?!cider:\/\/).+(music\.apple\.com)([^\s]+)/gi)
-            consola.info("[Link] Creating redirect embed.")
-            try {
-                fetch(link).catch(e => consola.error("[Link] Error creating redirect embed."))
-                    .then(result => result.text()).catch(e => null)
-                    .then(html => {
-                        const $ = cheerio.load(html)
-                        const title = $('meta[property="og:title"]').attr('content') || $('title').text() || $('meta[name="title"]').attr('content')
-                        const metadescription = $('meta[property="twitter:description"]').attr('content') || $('meta[name="twitter:description"]').attr('content')
-                        const description = metadescription.replace(/年年/g, "年")
-                        const image = $('meta[property="og:image"]').attr('content') || $('meta[property="og:image:url"]').attr('content')
-                        const modlink = link[0].replace('https://', '')
-                        const play_link = "https://cider.sh/p?" + modlink
-                        const view_link = "https://cider.sh/o?" + modlink
-                        const embed = new EmbedBuilder()
-                            .setColor('#fb003f')
-                            .setTitle(title)
-                            .setURL(link.toString())
-                            .setThumbnail(image)
-                            .setDescription(description)
-                            .setFooter({ text: "Shared by " + message.author.username, iconURL: message.author.avatarURL() })
-                            .setTimestamp()
-                        const interaction = new ActionRowBuilder()
-                            .addComponents(
-                                new ButtonBuilder()
-                                    .setLabel('Play In Cider')
-                                    .setStyle(ButtonStyle.Link)
-                                    .setURL(play_link),
-                                new ButtonBuilder()
-                                    .setLabel('View In Cider')
-                                    .setStyle(ButtonStyle.Link)
-                                    .setURL(view_link)
-                            )
-                        try {
-                            message.delete()
-                            return message.channel.send({ embeds: [embed], components: [interaction] });
-                        } catch (e) { consola.error(e) }
-                    }).catch(e => consola.error("[Link] Error creating redirect embed.", e))
-            } catch (e) { }
-        } else if (message.content.match(/(open\.spotify\.com)([^\s]+)/gi)) {
-            const link = message.content.match(/(open\.spotify\.com)([^\s]+)/gi)
-            try {
-                fetch("https://api.song.link/v1-alpha.1/links?url=" + link + "&userCountry=US").catch(e => consola.error("[Link] Error creating Spotify redirect embed."))
-                    .then(result => result.json()).catch(e => null)
-                    .then(json => {
-                        if (json.statusCode !== 400 && json.linksByPlatform.appleMusic) {
-                            const amlink = json.linksByPlatform.appleMusic.url
-                            fetch(amlink).catch(e => consola.error("[Link] Error creating redirect embed."))
-                                .then(result => result.text()).catch(e => null)
-                                .then(html => {
-                                    const $ = cheerio.load(html)
-                                    const title = $('meta[property="og:title"]').attr('content') || $('title').text() || $('meta[name="title"]').attr('content')
-                                    const metadescription = $('meta[property="twitter:description"]').attr('content') || $('meta[name="twitter:description"]').attr('content')
-                                    const description = metadescription.replace(/年年/g, "年")
-                                    const image = $('meta[property="og:image"]').attr('content') || $('meta[property="og:image:url"]').attr('content')
-                                    const modlink = amlink.replace('https://', '')
-                                    const play_link = "https://cider.sh/p?" + modlink
-                                    const view_link = "https://cider.sh/o?" + modlink
-                                    const embed = new EmbedBuilder()
-                                        .setColor('#fb003f')
-                                        .setTitle(title)
-                                        .setURL(amlink.toString())
-                                        .setThumbnail(image)
-                                        .setDescription(description)
-                                        .setFooter({ text: "Shared by " + message.author.username, iconURL: message.author.avatarURL() })
-                                        .setTimestamp()
-                                    const interaction = new ActionRowBuilder()
-                                        .addComponents(
-                                            new ButtonBuilder()
-                                                .setLabel('Play In Cider')
-                                                .setStyle(ButtonStyle.Link)
-                                                .setURL(play_link),
-                                            new ButtonBuilder()
-                                                .setLabel('View In Cider')
-                                                .setStyle(ButtonStyle.Link)
-                                                .setURL(view_link)
-                                        )
-                                    try {
-                                        message.delete()
-                                        return message.channel.send({ embeds: [embed], components: [interaction] });
-                                    } catch (e) { consola.error(e) }
-                                }).catch(e => null)
-                        } else {
-                            message.reply({ content: "Sorry, this song cannot be played in Cider. / Spotify Playlists are not currently supported yet" })
-                        }
-                    })
-            } catch (e) { consola.error(e) }
+        // /* Change Apple Music Link */
+        // if (message.content.match(/^(?!cider:\/\/).+(music\.apple\.com)([^\s]+)/gi)) {
+        //     const link = message.content.match(/^(?!cider:\/\/).+(music\.apple\.com)([^\s]+)/gi)
+        //     consola.info("[Link] Creating redirect embed.")
+        //     try {
+        //         fetch(link).catch(e => consola.error("[Link] Error creating redirect embed."))
+        //             .then(result => result.text()).catch(e => null)
+        //             .then(html => {
+        //                 const $ = cheerio.load(html)
+        //                 const title = $('meta[property="og:title"]').attr('content') || $('title').text() || $('meta[name="title"]').attr('content')
+        //                 const metadescription = $('meta[property="twitter:description"]').attr('content') || $('meta[name="twitter:description"]').attr('content')
+        //                 const description = metadescription.replace(/年年/g, "年")
+        //                 const image = $('meta[property="og:image"]').attr('content') || $('meta[property="og:image:url"]').attr('content')
+        //                 const modlink = link[0].replace('https://', '')
+        //                 const play_link = "https://cider.sh/p?" + modlink
+        //                 const view_link = "https://cider.sh/o?" + modlink
+        //                 const embed = new EmbedBuilder()
+        //                     .setColor('#fb003f')
+        //                     .setTitle(title)
+        //                     .setURL(link.toString())
+        //                     .setThumbnail(image)
+        //                     .setDescription(description)
+        //                     .setFooter({ text: "Shared by " + message.author.username, iconURL: message.author.avatarURL() })
+        //                     .setTimestamp()
+        //                 const interaction = new ActionRowBuilder()
+        //                     .addComponents(
+        //                         new ButtonBuilder()
+        //                             .setLabel('Play In Cider')
+        //                             .setStyle(ButtonStyle.Link)
+        //                             .setURL(play_link),
+        //                         new ButtonBuilder()
+        //                             .setLabel('View In Cider')
+        //                             .setStyle(ButtonStyle.Link)
+        //                             .setURL(view_link)
+        //                     )
+        //                 try {
+        //                     message.delete()
+        //                     return message.channel.send({ embeds: [embed], components: [interaction] });
+        //                 } catch (e) { consola.error(e) }
+        //             }).catch(e => consola.error("[Link] Error creating redirect embed.", e))
+        //     } catch (e) { }
+        // } else if (message.content.match(/(open\.spotify\.com)([^\s]+)/gi)) {
+        //     const link = message.content.match(/(open\.spotify\.com)([^\s]+)/gi)
+        //     try {
+        //         fetch("https://api.song.link/v1-alpha.1/links?url=" + link + "&userCountry=US").catch(e => consola.error("[Link] Error creating Spotify redirect embed."))
+        //             .then(result => result.json()).catch(e => null)
+        //             .then(json => {
+        //                 if (json.statusCode !== 400 && json.linksByPlatform.appleMusic) {
+        //                     const amlink = json.linksByPlatform.appleMusic.url
+        //                     fetch(amlink).catch(e => consola.error("[Link] Error creating redirect embed."))
+        //                         .then(result => result.text()).catch(e => null)
+        //                         .then(html => {
+        //                             const $ = cheerio.load(html)
+        //                             const title = $('meta[property="og:title"]').attr('content') || $('title').text() || $('meta[name="title"]').attr('content')
+        //                             const metadescription = $('meta[property="twitter:description"]').attr('content') || $('meta[name="twitter:description"]').attr('content')
+        //                             const description = metadescription.replace(/年年/g, "年")
+        //                             const image = $('meta[property="og:image"]').attr('content') || $('meta[property="og:image:url"]').attr('content')
+        //                             const modlink = amlink.replace('https://', '')
+        //                             const play_link = "https://cider.sh/p?" + modlink
+        //                             const view_link = "https://cider.sh/o?" + modlink
+        //                             const embed = new EmbedBuilder()
+        //                                 .setColor('#fb003f')
+        //                                 .setTitle(title)
+        //                                 .setURL(amlink.toString())
+        //                                 .setThumbnail(image)
+        //                                 .setDescription(description)
+        //                                 .setFooter({ text: "Shared by " + message.author.username, iconURL: message.author.avatarURL() })
+        //                                 .setTimestamp()
+        //                             const interaction = new ActionRowBuilder()
+        //                                 .addComponents(
+        //                                     new ButtonBuilder()
+        //                                         .setLabel('Play In Cider')
+        //                                         .setStyle(ButtonStyle.Link)
+        //                                         .setURL(play_link),
+        //                                     new ButtonBuilder()
+        //                                         .setLabel('View In Cider')
+        //                                         .setStyle(ButtonStyle.Link)
+        //                                         .setURL(view_link)
+        //                                 )
+        //                             try {
+        //                                 message.delete()
+        //                                 return message.channel.send({ embeds: [embed], components: [interaction] });
+        //                             } catch (e) { consola.error(e) }
+        //                         }).catch(e => null)
+        //                 } else {
+        //                     message.reply({ content: "Sorry, this song cannot be played in Cider. / Spotify Playlists are not currently supported yet" })
+        //                 }
+        //             })
+        //     } catch (e) { consola.error(e) }
             
             
-        /* Auto Replies */
-        } else if ((!message.member._roles.includes("848363050205446165") && !message.member._roles.includes("932811694751768656") && !message.member.id.includes("345021804210814976")) || overrideRegex.test(message.toString())) { // exclude dev team and donators
+        // /* Auto Replies */
+        // } else 
+        if ((!message.member._roles.includes("848363050205446165") && !message.member._roles.includes("932811694751768656") && !message.member.id.includes("345021804210814976")) || overrideRegex.test(message.toString())) { // exclude dev team and donators
             for (let reply of replies) {
                 var regex = new RegExp(`\\b${reply.name}\\b`, "gi");
                 if (regex.test(message.toString()) && !message.content.startsWith("https://")) {
