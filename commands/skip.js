5044b10194ba7c394fb213e46c4ccf4111f65d5b
yazninja <yazlesean@gmail.com>
2022-07-21 17:24:57 +0800

    add skip

---
+++ commands/skip.js
@@ -0,0 +1,25 @@
+import { SlashCommandBuilder } from 'discord.js';
+
+export const command = {
+    data: new SlashCommandBuilder()
+        .setName("skip")
+        .setDescription("Skip the current song"),
+    category: 'Music',
+    execute: async (interaction) => {
+        let { client } = await import('../index.js');
+        const musicordPlayer = client.musicordPlayer;
+        const queue = musicordPlayer.getQueue(interaction.guild);
+        const queueInfo = musicordPlayer.getQueueInfo(interaction.guild);
+        if (!queue.isPlaying) return await interaction.reply('There is no song playing!');
+        const member = interaction.guild.members.cache.get(interaction.member.user.id);
+
+        consola.info(queueInfo.voiceChannel.id)
+        if (queueInfo.voiceChannel.id === member.voice.channelId) {
+            queue.skip();
+            await interaction.reply("Song skipped!");
+        }
+        else {
+            await interaction.reply("You need to be in the same voice channel as the bot to skip the current song!");
+        }
+    }
+};
