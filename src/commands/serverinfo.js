7ba1a4579e96233173d59bb2f4bdf71fe8509e6b
yazninja <yazlesean@gmail.com>
2022-07-27 08:28:10 +0800

    try page buttons

--- src/commands/serverinfo.js
+++ src/commands/serverinfo.js
@@ -1,4 +1,4 @@
-import { EmbedBuilder, SlashCommandBuilder } from 'discord.js';
+import { EmbedBuilder, SlashCommandBuilder, ButtonBuilder, ActionRowBuilder, ButtonStyle } from 'discord.js';
 
 export const command = {
     data: new SlashCommandBuilder().setName("serverinfo").setDescription("Get info on the current server")
@@ -8,15 +8,27 @@ export const command = {
     category: 'General',
     execute: async (interaction) => {
         if (interaction.options.getSubcommand() === 'bans') {
+            let components = [];
             let bans = await interaction.guild.bans.fetch();
-            // consola.info("Bans:", bans);
-            await interaction.reply({ embeds: [new EmbedBuilder()
+            let bansArray = [...bans.values()]
+            let bansEmbed = new EmbedBuilder()
                 .setColor('Red')
                 .setTitle(`Bans on **${interaction.guild.name}**`)
                 .setThumbnail(interaction.guild.iconURL())
-                .setDescription(`${bans.size > 0 ? bans.map(ban => `||${ban.user.id}|| - **${ban.user.username}#${ban.user.discriminator}** - ${ban.reason || 'no reason provided'}`).join('\n') : 'No bans'}`)
-                .setFooter({ text: `Ban Count: ${bans.size}` })
-            ]});
+                .setDescription(`${bansArray.length > 0 ? bansArray.slice(0, 20).map((ban, i) => `${i + 1}.) **${ban.user.tag}**(||${ban.user.id}||) - ${ban.reason || 'no reason provided'}`).join('\n\n') : 'No bans'}`)
+                .setFooter({ text: `Total Ban Count: ${bans.size}` })
+            if(bansArray.length > 20) {
+                components = [new ActionRowBuilder().addComponents(
+                    new ButtonBuilder().setEmoji('⏪').setStyle(ButtonStyle.Secondary).setCustomId(`bansPages|${Math.floor(bansArray / 20 + 1)}`),
+                    new ButtonBuilder().setEmoji('⏩').setStyle(ButtonStyle.Secondary).setCustomId(`bansPages|2`)
+                )]
+            }
+            consola.info(bansArray)
+            // consola.info("Bans:", bans);
+            await interaction.reply({
+                embeds: [bansEmbed],
+                components
+            });
         }
     }
-}
+} 
