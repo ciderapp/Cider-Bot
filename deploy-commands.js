091c023f3c2b07ec867fdf68ad18f305e9e05116
yazninja <71800112+yazninja@users.noreply.github.com>
2022-07-29 12:33:19 +0800

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,21 +1,25 @@
-import { token, clientId } from './src/local.js';
+import 'dotenv/config';
 import { readdirSync } from 'fs';
 import { REST } from '@discordjs/rest'
 import { Routes } from 'discord.js'
 
 const commands = [];
-const commandFiles = readdirSync('./src/commands').filter(file => file.endsWith('.js'));
-for (const file of commandFiles) {
-    const { command } = await import(`./src/commands/${file}`);
-    commands.push(command.data);
+
+const commandFolders = readdirSync('./src/commands/');
+for (const folder of commandFolders) {
+    const commandFiles = readdirSync(`./src/commands/${folder}`).filter(file => file.endsWith('.js'));
+    for (const file of commandFiles) {
+        const { command } = await import(`./src/commands/${folder}/${file}`);
+        commands.push(command.data);
+    }
 }
 
-const rest = new REST({ version: '10' }).setToken(token);
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
 (async () => {
     try {
         console.log('Started refreshing application (/) commands.');
-        await rest.put(Routes.applicationCommands(clientId),{ body: commands });
+        await rest.put(Routes.applicationCommands(process.env.AUTH_CLIENTID),{ body: commands });
         console.log('Successfully reloaded application (/) commands.\n' + commands.map(c => c.name).join(', '));
     } catch (error) {
         console.error(error);
