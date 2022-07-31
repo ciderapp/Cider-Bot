65a51aa9bb4a6be4084b59d3661a147200f36bac
yazninja <71800112+yazninja@users.noreply.github.com>
2022-08-01 02:36:04 +0800

    use nee variable

--- src/integrations/musickitAPI.js
+++ src/integrations/musickitAPI.js
@@ -1,5 +1,7 @@
+import 'dotenv/config'
+
 export const getAPIToken = async () => {
-    let apiToken = await fetch("https://api.cider.sh/v1", { headers: { "User-Agent": "Cider" } });
+    let apiToken = await fetch("https://api.cider.sh/v1", { headers: { process.env.API_HEADER_KEY: process.env.API_HEADER_VALUE } });
     apiToken = await apiToken.json()
     return apiToken.token;
 }
