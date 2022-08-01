1f21ec15f810277794c27ec230c21bb6ec246f5e
yazninja <yazlesean@gmail.com>
2022-08-01 16:44:47 +0800

    try skip vote v2

--- src/commands/music/skip.js
+++ src/commands/music/skip.js
@@ -15,7 +15,7 @@ export const command = {
         if (!interaction.member.voice.channelId) return await interaction.reply({ content: "You need to be in a voice channel to use this command!", ephemeral: true });
         if (interaction.guild.members.me.voice.channelId && interaction.member.voice.channelId !== interaction.guild.members.me.voice.channelId) return await interaction.reply({ content: "You are not in my voice channel!", ephemeral: true });
         if (interaction.guildId == process.env.guildId && interaction.channelId != "843954941827481670") return await interaction.reply({ content: "This command can only be used in the <#843954941827481670> channel!", ephemeral: true });
-        if (interaction.guildId == process.env.guildId && await interaction.member.roles.cache.findKey(role => role.test(mod)) === undefined && (queue.metadata.voteskip < 3 || interaction.guild.members.me.voice.channel.members.size < 5)) {
+        if (interaction.guildId == process.env.guildId && await interaction.member.roles.cache.findKey(role => role.id.test(mod)) === undefined && (queue.metadata.voteskip < 3 || interaction.guild.members.me.voice.channel.members.size < 5)) {
             queue.metadata.voteskip++;
             return await interaction.reply(`You need at least 3 votes to skip the current song! (${queue.metadata.voteskip}/${3})`);
         }
