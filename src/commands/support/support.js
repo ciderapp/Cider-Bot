86792050c53a2764991f83447fc4c99307d60e6c
Monochromish <79590499+Monochromish@users.noreply.github.com>
2022-09-22 15:35:43 +1000

    Remove support and ticket channel buttons

--- src/commands/support/support.js
+++ src/commands/support/support.js
@@ -7,10 +7,8 @@ export const command = {
         let embed = new EmbedBuilder()
             .setColor('Random')
             .setTitle("Support")
-            .setDescription("Need support? Or want to request for data deletion? Create a post in the <#996168362826674246> channel or contact us on given platforms.");
+            .setDescription("Need support? Or want to request for data deletion? Contact us on given platforms.");
         let components = [new ActionRowBuilder().addComponents(
-            new ButtonBuilder().setLabel('Support Channel').setStyle(ButtonStyle.Link).setURL('https://discord.com/channels/843954443845238864/996168362826674246'), // Discussion Channel Link
-            new ButtonBuilder().setLabel('Ticket Support').setStyle(ButtonStyle.Link).setURL('https://discord.com/channels/843954443845238864/872561715110367302'), // Ticket Channel Link
             new ButtonBuilder().setLabel('Mail').setStyle(ButtonStyle.Link).setURL('mailto:cryptofyre@cider.sh'),
             new ButtonBuilder().setLabel('GitHub').setStyle(ButtonStyle.Link).setURL('https://github.com/orgs/ciderapp/discussions'),
             new ButtonBuilder().setLabel('Twitter').setStyle(ButtonStyle.Link).setURL('https://twitter.com/useCider/'),
