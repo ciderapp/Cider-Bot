739e5b9e6d3682aab51d7493d8f4fe7a846f440c
yazninja <yazlesean@gmail.com>
2022-09-22 17:09:38 +0800

    fix support.js

--- src/commands/support/support.js
+++ src/commands/support/support.js
@@ -1,4 +1,4 @@
-import {SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle} from 'discord.js';
+import { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } from 'discord.js';
 
 export const command = {
     data: new SlashCommandBuilder().setName('support').setDescription("Need support?").addUserOption(option => option.setName('user').setDescription('User to repond to')),
@@ -8,17 +8,16 @@ export const command = {
             .setColor('Random')
             .setTitle("Support")
             .setDescription("Need support? Or want to request for data deletion? Contact us on given platforms.");
-        let components = [new ActionRowBuilder().addComponents(
-            new ButtonBuilder().setLabel('Mail').setStyle(ButtonStyle.Link).setURL('mailto://cryptofyre@cider.sh'),
+        let component = new ActionRowBuilder().addComponents(
+            // new ButtonBuilder().setLabel('Mail').setStyle(ButtonStyle.Link).setURL('mailto://cryptofyre@cider.sh'),
             new ButtonBuilder().setLabel('GitHub').setStyle(ButtonStyle.Link).setURL('https://github.com/orgs/ciderapp/discussions'),
             new ButtonBuilder().setLabel('Twitter').setStyle(ButtonStyle.Link).setURL('https://twitter.com/useCider/'),
-        )];
+        );
         let user = interaction.options.getUser('user') || null
         if (user) {
-            await interaction.reply({ content: `${user}`, embeds: [embed], components: [components] });
+            await interaction.reply({ content: `${user}`, embeds: [embed], components: [component] });
         } else {
-            await interaction.reply({ embeds: [embed], components: [components], ephemeral: true });
+            await interaction.reply({ embeds: [embed], components: [component], ephemeral: true });
         }
-        await interaction.reply({ embeds: [embed], components: [components] });
     },
 };
