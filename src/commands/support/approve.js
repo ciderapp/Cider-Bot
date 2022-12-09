a3246d1b56cb23bb0af170a0a4b7a84105598c37
Swiftzerr <elliotjarnit@gmail.com>
2022-12-09 07:43:07 -0800

    alpha channel approve

--- src/commands/support/approve.js
+++ src/commands/support/approve.js
@@ -1,7 +1,7 @@
 import { SlashCommandBuilder, EmbedBuilder } from 'discord.js';
 
 export const command = {
-    data: new SlashCommandBuilder().setName('approve').setDescription('Approve a user as donator').addUserOption(option => option.setName('user').setDescription('User to approve').setRequired(true)).addStringOption(option => option.setName('platform').setDescription('Platform user donated on').setRequired(true).addChoices({ name: 'OpenCollective', value: 'oc' }, { name: 'Kofi', value: 'kofi' }, { name: 'GitHub', value: 'github' })),
+    data: new SlashCommandBuilder().setName('approve').setDescription('Approve a user as donator').addUserOption(option => option.setName('user').setDescription('User to approve').setRequired(true)).addStringOption(option => option.setName('platform').setDescription('Platform user donated on').setRequired(true).addChoices({ name: 'OpenCollective', value: 'oc' }, { name: 'Kofi', value: 'kofi' }, { name: 'GitHub', value: 'github' }, { name: 'Alpha Channel', value: 'alpha' })),
     category: 'Support',
     async execute(interaction) {
         if (!interaction.member._roles.includes('928845681517535323') || !interaction.member._roles.includes('848363050205446165')) {
@@ -21,6 +21,9 @@ export const command = {
                     break;
                 case 'github':
                     await interaction.guild.members.cache.get(user.id).roles.add("990362567241236490");
+                    break;
+                case 'alpha':
+                    await interaction.guild.members.cache.get(user.id).roles.add("1050089837648162886");
                     break;
             }
 
