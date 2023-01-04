6582c3188f47ba0cb3521531dfe230015e1512de
yazninja <yazlesean@gmail.com>
2023-01-05 02:03:29 +0800

    mongo -> firebase

--- src/commands/general/time.js
+++ src/commands/general/time.js
@@ -1,5 +1,5 @@
 import { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } from 'discord.js';
-import { mongo } from '../../integrations/mongo.js';
+import { firebase } from '../../integrations/firebase.js';
 
 export const command = {
     data: new SlashCommandBuilder().setName('time').setDescription("Displays user's local time").addUserOption(option => option.setName('user').setDescription('Mention user')),
@@ -7,7 +7,7 @@ export const command = {
     async execute(interaction) {
         let user = interaction.options.getUser('user') || interaction.user;
 
-        await mongo.getUserTimezone(user.id).then(async (timezone) => {
+        await firebase.getUserTimezone(user.id).then(async (timezone) => {
             if (!timezone) {
                 (user.id == interaction.user.id) ? await interaction.reply({ content: 'You have not set your timezone yet. Use `/settimezone` to set your timezone.', ephemeral: true }) : await interaction.reply({ content: `${user.tag} has not set their timezone yet.`, ephemeral: true });
             } else {
