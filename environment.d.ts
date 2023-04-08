e2491f2c5618d6fbae3dee203e68d0bf2685944f
yazninja <yazlesean@gmail.com>
2023-04-08 18:18:55 +0800

    hotflix for songs not playing
    -used play-dl as stream extractor

--- environment.d.ts
+++ environment.d.ts
@@ -4,6 +4,7 @@ declare global {
       NODE_ENV: "development" | "production";
       DISCORD_TOKEN: string;
       DISCORD_ID: string;
+      DP_FORCE_YTDL_MODE: "play-dl" | "ytdl-core";
     }
   }
 }
