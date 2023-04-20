2e2fd9e22748e888b9c541da5ed3bf468f7ebb7d
yazninja <yazlesean@gmail.com>
2023-04-20 18:02:14 +0300

    add error handling logs

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-import { Client, GatewayIntentBits, Partials, Events, User, Collection } from 'discord.js' ;
+import { Client, GatewayIntentBits, Partials, Events, User, Collection, TextChannel, resolveColor, EmbedBuilder, Colors } from 'discord.js';
 import 'dotenv/config';
 import consola from 'consola';
 import { readdirSync } from 'fs';
 import { Player } from 'discord-player';
 
-declare module "discord.js" {
+declare module 'discord.js' {
     export interface Client {
-        amAPIToken: string
-        commands: Collection<unknown, any>
-        canPingKeefe: boolean
-        interactions: Collection<unknown, any>
-        replies: Collection<unknown, any>
-        player: Player
-        playerEmbeds: Map<string, any>
+        amAPIToken: string;
+        commands: Collection<unknown, any>;
+        canPingKeefe: boolean;
+        interactions: Collection<unknown, any>;
+        replies: Collection<unknown, any>;
+        player: Player;
+        playerEmbeds: Map<string, any>;
     }
 }
 
@@ -25,14 +25,9 @@ const client = new Client({
         GatewayIntentBits.GuildVoiceStates,
         GatewayIntentBits.DirectMessages,
         GatewayIntentBits.DirectMessageReactions,
-        GatewayIntentBits.DirectMessageTyping,
+        GatewayIntentBits.DirectMessageTyping
     ],
-    partials: [
-        Partials.Channel,
-        Partials.User,
-        Partials.GuildMember,
-        Partials.Reaction
-    ]
+    partials: [Partials.Channel, Partials.User, Partials.GuildMember, Partials.Reaction]
 });
 client.playerEmbeds = new Map();
 client.player = new Player(client);
@@ -45,27 +40,27 @@ const fileType = process.env.NODE_ENV === 'development' ? '.ts' : '.js';
 /** REGISTER COMMANDS */
 const commandFolders = readdirSync(`./${mainFolder}/commands/`);
 for (const folder of commandFolders) {
-    const commandFiles = readdirSync(`./${mainFolder}/commands/${folder}`).filter(file => file.endsWith(fileType));
+    const commandFiles = readdirSync(`./${mainFolder}/commands/${folder}`).filter((file) => file.endsWith(fileType));
     for (const file of commandFiles) {
         const { command } = await import(`./commands/${folder}/${file}`);
         client.commands.set(command.data.name, command);
-        consola.info("\x1b[32m%s\x1b[0m", "Registered Command:", command.data.name, command?.category);
+        consola.info('\x1b[32m%s\x1b[0m', 'Registered Command:', command.data.name, command?.category);
     }
 }
 
 /** REGISTER INTERACTIONS */
-const interactionFiles = readdirSync(`./${mainFolder}/interactions`).filter(file => file.endsWith(fileType));
+const interactionFiles = readdirSync(`./${mainFolder}/interactions`).filter((file) => file.endsWith(fileType));
 for (const file of interactionFiles) {
     const { interaction } = await import(`./interactions/${file}`);
     client.interactions.set(interaction.data.name, interaction);
-    consola.info("\x1b[32m%s\x1b[0m", "Registered Interaction:", interaction.data.name);
+    consola.info('\x1b[32m%s\x1b[0m', 'Registered Interaction:', interaction.data.name);
 }
 
-const eventFiles = readdirSync(`./${mainFolder}/events`).filter(file => file.endsWith(fileType));
+const eventFiles = readdirSync(`./${mainFolder}/events`).filter((file) => file.endsWith(fileType));
 for (const file of eventFiles) {
-    const event  = (await import(`./events/${file}`)).default;
+    const event = (await import(`./events/${file}`)).default;
     consola.info(`Loaded event: ${event.name}`);
-    if(event?.devOnly && process.env.NODE_ENV !== 'development') continue;
+    if (event?.devOnly && process.env.NODE_ENV !== 'development') continue;
     if (event?.once) {
         client.once(event.name, (...args) => event.execute(...args));
     } else {
@@ -73,21 +68,26 @@ for (const file of eventFiles) {
     }
 }
 
-
 client.login(process.env.DISCORD_TOKEN);
 
 /*** ERROR HANDLING ***/
-process.on('unhandledRejection', (error : Error) => {
-    consola.info("Unhandled Rejection")
+process.on('unhandledRejection', (error: Error) => {
+    consola.info('Unhandled Rejection');
     consola.error(error);
     consola.error(error.stack);
-    // let errorEmbed = { color: resolveColor("Red"), title: "Error", description: `${error.name}`, fields: [{ name: 'Message', value: `${error.message}` }, { name: 'Origin', value: `${error.stack}` }] }
-    // client.channels.cache.get(process.env.errorChannel).send({ content: `Unhandled Rejection`, embeds: [errorEmbed] })
-})
-process.on('uncaughtException', (error : Error)  => {
-    consola.info("Uncaught Exception")
+    (client.channels.cache.get(CiderGuildChannels.botLog) as TextChannel)?.send({ content: `Unhandled Rejection`, embeds: [createErrorEmbed(error)] });
+});
+process.on('uncaughtException', (error: Error) => {
+    consola.info('Uncaught Exception');
     consola.error(error);
     consola.error(error.stack);
-    // let errorEmbed = { color: resolveColor("Red"), title: "Error", description: `${error.name}`, fields: [{ name: 'Message', value: `${error.message}` }, { name: 'Origin', value: `${error.stack}` }] }
-    // client.channels.cache.get(process.env.errorChannel).send({ content: `Uncaught Exception`, embeds: [errorEmbed] })
-})
+    (client.channels.cache.get(CiderGuildChannels.botLog) as TextChannel)?.send({ content: `Uncaught Exception`, embeds: [createErrorEmbed(error)] });
+});
+
+function createErrorEmbed(error: Error) {
+    return new EmbedBuilder()
+        .setColor(Colors.Red)
+        .setTitle(error.name)
+        .setDescription(error.message) //@ts-ignore
+        .addFields({ name: 'Origin', value: `\`\`\`${error.stack?.slice(0, 1024)}\`\`\`` });
+}
