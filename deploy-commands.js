0eadaa48d25c1fb1f3cd917a7b574057e2c6b6b5
yazninja <71800112+yazninja@users.noreply.github.com>
2023-04-25 17:35:57 +0300

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -7,21 +7,18 @@ const commands = [];
 
 const commandFolders = readdirSync('./src/commands/');
 for (const folder of commandFolders) {
-    const commandFiles = readdirSync(`./src/commands/${folder}`).filter(file => file.endsWith('.js'));
+    const commandFiles = readdirSync(`./build/commands/${folder}`).filter(file => file.endsWith('.js'));
     for (const file of commandFiles) {
-        const { command } = await import(`./src/commands/${folder}/${file}`);
+        const { command } = await import(`./build/commands/${folder}/${file}`);
         commands.push(command.data);
     }
 }
 
 const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
-
-(async () => {
-    try {
-        console.log('Started refreshing application (/) commands.');
-        await rest.put(Routes.applicationCommands(process.env.DISCORD_ID),{ body: commands });
-        console.log('Successfully reloaded application (/) commands.\n' + commands.map(c => c.name).join(', '));
-    } catch (error) {
-        console.error(error);
-    }
-})();
+try {
+    console.log('Started refreshing application (/) commands.');
+    await rest.put(Routes.applicationCommands(process.env.DISCORD_ID), { body: commands });
+    console.log('Successfully reloaded application (/) commands.\n' + commands.map(c => c.name).join(', '));
+} catch (error) {
+    console.error(error);
+}
