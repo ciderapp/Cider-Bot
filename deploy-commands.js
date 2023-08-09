36a259476608ad17ef2c1b182364b74fe36e61e7
yazninja <71800112+yazninja@users.noreply.github.com>
2023-08-10 02:27:25 +0300

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,13 +1,12 @@
 import 'dotenv/config';
 import { readdirSync } from 'fs';
-import { REST } from '@discordjs/rest'
-import { Routes } from 'discord.js'
+import { REST, Routes } from 'discord.js';
 
 const commands = [];
 
 const commandFolders = readdirSync('./src/commands/');
 for (const folder of commandFolders) {
-    const commandFiles = readdirSync(`./build/commands/${folder}`).filter(file => file.endsWith('.js'));
+    const commandFiles = readdirSync(`./build/commands/${folder}`).filter((file) => file.endsWith('.js'));
     for (const file of commandFiles) {
         const { command } = await import(`./build/commands/${folder}/${file}`);
         commands.push(command.data);
@@ -18,7 +17,7 @@ const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 try {
     console.log('Started refreshing application (/) commands.');
     await rest.put(Routes.applicationCommands(process.env.DISCORD_ID), { body: commands });
-    console.log('Successfully reloaded application (/) commands.\n' + commands.map(c => c.name).join(', '));
+    console.log('Successfully reloaded application (/) commands.\n' + commands.map((c) => c.name).join(', '));
 } catch (error) {
     console.error(error);
 }
